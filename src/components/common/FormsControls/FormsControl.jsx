0a135aab2b52b0be9f4b6ca0ab500768416d8c91
woodoo2013 <woodoo2013@gmail.com>
2021-01-27 19:12:33 +0700

    рефакторинг FormsControl

--- src/components/common/FormsControls/FormsControl.jsx
+++ src/components/common/FormsControls/FormsControl.jsx
@@ -2,26 +2,24 @@ import React from 'react'
 import style from './FormsControl.module.css'
 
 
-export const Textarea = ({input, meta, ...props}) => {
+export const FormControl = ({input, meta, ...props}) => {
     let hasError = meta.error && meta.touched
     return (
         <div className={style.formsControl + ' ' + (hasError ? style.error : '')}>
             <div >
-                <textarea  {...input} {...props} />
+                {props.children}
             </div>
             { hasError && <span>{meta.error}</span>}
         </div>
     )
 }
 
-export const Input = ({input, meta, ...props}) => {
-    let hasError = meta.error && meta.touched
-    return (
-        <div className={style.formsControl + ' ' + (hasError ? style.error : '')}>
-            <div >
-                <input  {...input} {...props} />
-            </div>
-            { hasError && <span>{meta.error}</span>}
-        </div>
-    )
+export const Textarea = (props) => {
+    const {input, meta, child,...restProps} = props
+    return <FormControl {...props}><textarea {...input} {...restProps}/></FormControl>
+}
+
+export const Input = (props) => {
+    const {input, meta, child,...restProps} = props
+    return <FormControl {...props}><input {...input} {...restProps}/></FormControl>
 }
