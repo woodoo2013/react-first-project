729f9e91e6d95fdbd727030244ec12db5856be4e
woodoo2013 <woodoo2013@gmail.com>
2021-01-30 16:37:13 +0700

    selectors
    reselect install

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -9,6 +9,14 @@ import {
     getUsers,
 } from "../../redux/users-reducer";
 import Loader from "../common/Loader/Loader";
+import {
+    followingInProgress,
+    getCurrentPage,
+    getIsFetching,
+    getPageSize,
+    getTotalUsersCount,
+    getUsersSelector
+} from "../../redux/users-selectors";
 
 
 class UsersContainer extends React.Component {
@@ -39,12 +47,12 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgress: state.usersPage.followingInProgress
+        users: getUsersSelector(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgress: followingInProgress(state)
     }
 }
 
---
+++ src/redux/users-selectors.js
@@ -0,0 +1,18 @@
+export let getUsersSelector = (state) => {
+    return state.usersPage.users
+}
+export let getPageSize = (state) => {
+    return state.usersPage.pageSize
+}
+export let getTotalUsersCount = (state) => {
+     return state.usersPage.totalUsersCount
+}
+export let getCurrentPage = (state) => {
+    return state.usersPage.currentPage
+}
+export let getIsFetching = (state) => {
+    return state.usersPage.isFetching
+}
+export let followingInProgress = (state) => {
+    return state.usersPage.followingInProgress
+}
